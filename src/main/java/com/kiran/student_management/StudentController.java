697b80f9542c215422816a81869100027482a35d
kiran22022002 <54470122+kiran22022002@users.noreply.github.com>
2024-04-18 22:34:27 +0530

    new end point

--- src/main/java/com/kiran/student_management/StudentController.java
+++ src/main/java/com/kiran/student_management/StudentController.java
@@ -1,6 +1,8 @@
 package com.kiran.student_management;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,6 +10,10 @@ public class StudentController {
     @GetMapping("/home")
     public  String hey(){
         return "hello bhai";
+    }
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public  String getLost(){
+        return "get lost";
     }
 
 }
