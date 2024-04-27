1ac06f186ed0ea24343a1ac19c716c66e92592e1
kiran22022002 <54470122+kiran22022002@users.noreply.github.com>
2024-04-27 21:42:27 +0530

    added seperate package for controllers

--- src/main/java/com/kiran/student_management/controller/ExampleStudent.java
+++ src/main/java/com/kiran/student_management/controller/ExampleStudent.java
@@ -1,2 +1,14 @@
-package com.kiran.student_management.controller;public class ExampleStudent {
+package com.kiran.student_management.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExampleStudent {
+
+    @RequestMapping("/example")
+    public  String go(){
+        return "go";
+    }
 }
